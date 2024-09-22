2f23a8d02a9d3a61d0e1ef1d2a6bed2950714592
Marc <2023_marc.rivero@iticbcn.cat>
2024-09-22 15:11:17 +0200

    Activitat 1

--- Rot13/Rot13.java
+++ Rot13/Rot13.java
@@ -4,7 +4,7 @@
 public class Rot13 {
     private static final String abc = "A,Á,À,B,C,Ç,D,E,É,È,F,G,H,I,Í,Ï,J,K,L,M,N,Ñ,O,Ó,Ò,P,Q,R,S,T,U,Ú,Ü,V,W,X,Y,Z";
     
-    String[] abcMinus = abc.toLowerCase().split(",");
+    String[] abcLower = abc.toLowerCase().split(",");
     String[] abcUpper = abc.toUpperCase().split(",");
 
     public String xifraRot13(String cadena){
@@ -18,9 +18,9 @@ public class Rot13 {
                         }
                     }
                 }else{
-                    for(int y = 0; y < abcMinus.length; y++){
-                        if(abcMinus[y].equals(Character.toString(cadena.charAt(i)))){
-                            xifrat = xifrat + abcMinus[(y + 13)%abcMinus.length];
+                    for(int y = 0; y < abcLower.length; y++){
+                        if(abcLower[y].equals(Character.toString(cadena.charAt(i)))){
+                            xifrat = xifrat + abcLower[(y + 13)%abcLower.length];
                         }
                     }
                 }
@@ -36,13 +36,13 @@ public class Rot13 {
                 if(Character.isUpperCase(cadena.charAt(i))){
                     for(int x = 0; x < abcUpper.length; x++){
                         if(abcUpper[x].equals(Character.toString(cadena.charAt(i)))){
-                            desxifrat = desxifrat + abcUpper[(x - 13)%abcUpper.length];
+                            desxifrat = desxifrat + abcUpper[Math.abs((x - 13)%abcUpper.length)];
                         }
                     }
                 }else{
-                    for(int y = 0; y < abcMinus.length; y++){
-                        if(abcMinus[y].equals(Character.toString(cadena.charAt(i)))){
-                            desxifrat = desxifrat + abcMinus[(y - 13)%abcMinus.length];
+                    for(int y = 0; y < abcLower.length; y++){
+                        if(abcLower[y].equals(Character.toString(cadena.charAt(i)))){
+                            desxifrat = desxifrat + abcLower[Math.abs((y - 13)%abcLower.length)];
                         }
                     }
                 }
